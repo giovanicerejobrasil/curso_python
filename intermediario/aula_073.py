6fae84cf721d8ee3cf967b93e791815f80e4a760
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-09-19 23:05:03 -0300

    add: retorno de funções

---
+++ intermediario/aula_073.py
@@ -0,0 +1,13 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+  return x + y
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+
+print(soma1)
+print(soma2)
+print(soma(soma1, soma2))
