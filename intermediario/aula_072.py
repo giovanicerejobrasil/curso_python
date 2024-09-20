0ecbb48130064eced805576cd5608e9ed2d5bc14
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-09-19 22:44:52 -0300

    adjust: atualização do comentário sobre escopos

--- intermediario/aula_072.py
+++ intermediario/aula_072.py
@@ -5,6 +5,10 @@ Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
+Não temos acesso a nomes de escopos internos nos escopos
+externos.
+A palavra global faz uma variável do escopo externo 
+ser a mesma no escopo interno.
 """
 
 x = 1
