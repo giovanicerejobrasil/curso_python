bcc30240a5e9f73d02b4b51989142ec3b3513400
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-10-05 19:09:59 -0300

    add: filter

--- intermediario/aula_114.py
+++ intermediario/aula_114.py
@@ -3,9 +3,19 @@ filter - é um filtro funcional
 """
 from dados.produtos_module import products
 
-def print_iterator(iterator):
+def print_iterator(name, iterator):
+  print(name)
   print(*iterator, sep='\n')
 
 def print_division(num_repeat=50):
   print('', '=' * num_repeat, '', sep='\n')
   
+new_products = filter(
+  lambda product: product['price'] > 50,
+  products
+)
+
+print_iterator('PRODUTOS', products)
+print_division()
+
+print_iterator('NOVOS PRODUTOS', new_products)
