e023ea1a4f5cc808e1330b81082ccae99ede1e2d
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-10-30 16:17:32 -0300

    add: DocString de múltiplas linhas

--- poo/aula_159/main.py
+++ poo/aula_159/main.py
@@ -1,8 +1,25 @@
 import one_line
+import multiple_lines
 
 
+def print_division(text='', sep_text='=', num_repeat=35, jump=False):
+    print() if jump else ''
+    print(sep_text * num_repeat, sep='\n')
+    print(text, end='\n\n')
+
+
+print_division('DOCSTRING DE UMA LINHA')
 print(dir(one_line))
 print(one_line.__doc__)
 print(one_line.__file__)
+print()
 print(one_line.__name__)
-help(one_line)
+# help(one_line)
+
+print_division('DOCSTRING DE VÁRIAS LINHAS', jump=True)
+print(dir(multiple_lines))
+print(multiple_lines.__doc__)
+print(multiple_lines.__file__)
+print()
+print(multiple_lines.__name__)
+help(multiple_lines)
---
+++ poo/aula_159/multiple_lines.py
@@ -0,0 +1,34 @@
+"""
+DOCUMENTAÇÃO DO MÓDULO
+
+Aqui você conta tudo o que é feito no código
+ISTO É UMA DESCRIÇÃO
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta est nisi, 
+sit amet gravida dolor ornare eget. Quisque purus ligula, interdum id faucibus 
+nec, dictum vitae nibh. Nullam rhoncus orci leo, et rutrum neque ultricies et. 
+Duis dapibus leo ac quam fringilla, at dictum eros iaculis. In maximus sit amet 
+felis non lacinia. Aliquam sapien nunc, euismod vitae lacinia rutrum, volutpat 
+eget lorem. Pellentesque bibendum fringilla sapien ullamcorper sagittis. Duis 
+interdum sapien id tortor commodo consequat. Sed dictum metus at est porta, 
+iaculis consequat justo lacinia. Nullam urna diam, tincidunt non condimentum ac, 
+eleifend et nisl. Nulla tincidunt justo eu lacus scelerisque tincidunt. Aliquam 
+id justo sed augue volutpat scelerisque.
+
+Cras laoreet non ligula vitae sollicitudin. Sed fermentum dui nec erat efficitur 
+ullamcorper. Donec id nunc ligula. Nam vitae sollicitudin enim. Curabitur 
+egestas, ex non consequat venenatis, metus lorem facilisis nulla, sed dictum mi 
+orci in odio. Morbi nec enim a dui blandit euismod. In hac habitasse platea 
+dictumst. Cras enim leo, eleifend vitae ante ut, commodo auctor augue. Aliquam 
+erat volutpat.
+
+Quisque a turpis sed lacus efficitur hendrerit. Etiam fringilla urna sed magna 
+suscipit, non luctus mauris egestas. Nam sagittis lacus purus, a varius nibh 
+bibendum non. Donec vehicula facilisis quam, ac volutpat felis mattis sed. Etiam 
+eleifend fringilla arcu vitae semper. Sed nec ex enim. Morbi tristique risus 
+felis, eget euismod orci sagittis vel. Phasellus pretium a sem vel fringilla. 
+Sed tempor auctor interdum. Suspendisse dolor nisl, vehicula in purus sed, 
+tincidunt ullamcorper sapien. Praesent tincidunt magna eget dolor rutrum 
+interdum et quis nulla. Nulla vitae erat mi. Class aptent taciti sociosqu ad 
+litora torquent per conubia nostra, per inceptos himenaeos.
+"""
