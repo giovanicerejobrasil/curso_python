fca9fbe3a127bc9198cd7648b3fb8f2f63a99fac
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-11-02 12:18:24 -0300

    add: execuções de testes apenas dentro do módulo

---
+++ 04_modulos/aula_164/app.py
@@ -0,0 +1,9 @@
+"""
+Execuções de testes apenas dentro do módulo em questão
+
+Utilizamos:
+if __name__ == '__main__':
+"""
+from modulo import sum_
+
+print(sum_(75, 32))
---
+++ 04_modulos/aula_164/modulo.py
@@ -0,0 +1,12 @@
+def sum_(x: float, y: float) -> float:
+    return x + y
+
+
+def main() -> None:
+    print(__name__)
+    print(sum_(11, 22))
+    print(sum_(97, 59))
+
+
+if __name__ == '__main__':
+    main()
