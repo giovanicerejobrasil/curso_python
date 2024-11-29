2f07beeff9191d112d2ff03a889e1f48a1924fa9
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-11-29 20:39:00 -0300

    add: teoria dos métodos http

---
+++ 04_modulos/aula_192.py
@@ -0,0 +1,28 @@
+"""
+(Parte 1) Básico do protocolo HTTP (HyperText Transfer Protocol)
+
+HTTP (HyperText Transfer Protocol) é um protocolo usado enviar e receber
+dados na Internet. Ele funciona no modo cliente/servidor, onde o cliente
+(seu navegador, por exemplo) faz uma requisição ao servidor
+(site, por exemplo), que responde com os dados adequados.
+
+A mensagem de requisição do cliente deve incluir dados como:
+- O método HTTP
+    - leitura (safe) - GET, HEAD (cabeçalhos), OPTIONS (métodos suportados)
+    - escrita - POST, PUT (substitui), PATCH (atualiza), DELETE
+- O endereço do recurso a ser acessado (/users/)
+- Os cabeçalhos HTTP (Content-Type, Authorization)
+- O Corpo da mensagem (caso necessário, de acordo com o método HTTP)
+
+A mensagem de resposta do servidor deve incluir dados como:
+- código de status HTTP (200 success, 404 Not found, 301 Moved Permanently)
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+- Os cabeçalhos HTTP (Content-Type, Accept)
+- O corpo da mensagem (Pode estar em vazio em alguns casos)
+
+Executar um servidor para HTML + CSS + JS com python:
+- venv/bin/python -m http.server 3333 -d 04_modulos/aula_192_site/ -b "::"
+-> localhost por ipv6
+- venv/bin/python -m http.server 3333 -d 04_modulos/aula_192_site/ -b 127.0.0.1
+-> localhost por ipv4
+"""
