4f72f849fbb50314afafbeaad35f0abe3717fa29
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-12-03 14:16:28 -0300

    add: Jupyter Notebook

---
+++ 04_modulos/aula_197.py
@@ -0,0 +1,12 @@
+"""
+Jupyter Notebook - Instalação e teste
+"""
+
+# Cria uma variável 'a'
+a = 2
+
+# Cria uma variável 'b' que recebe o valor da variável 'a' em dobro
+b = a * 2
+
+# Mostrao valor de 'a' + 'b'
+print(a + b)
